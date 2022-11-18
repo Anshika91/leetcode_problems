5029e4e6254bfe496aa0449d6749db11f1a8e233
Anshika Mishra <92078327+Anshika91@users.noreply.github.com>
2022-11-18 22:26:03 +0530

    Time: 700 ms (76.47%), Space: 0B (100.00%) - LeetHub

---
+++ 0511-game-play-analysis-i/0511-game-play-analysis-i.sql
@@ -0,0 +1,9 @@
+# Write your MySQL query statement below
+# Time:  O(n)
+# Space: O(n)
+
+SELECT player_id, 
+       Min(event_date) first_login 
+FROM   activity 
+GROUP  BY player_id 
+ORDER  BY NULL 
