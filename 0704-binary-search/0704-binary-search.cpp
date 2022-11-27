d7d29b7035755873889ff6b5bf2d1ab9a224d498
Anshika Mishra <92078327+Anshika91@users.noreply.github.com>
2022-11-27 09:35:26 +0530

    Time: 132 ms (5.21%), Space: 27.6 MB (64.21%) - LeetHub

--- 0704-binary-search/0704-binary-search.cpp
+++ 0704-binary-search/0704-binary-search.cpp
@@ -1,18 +1,19 @@
 class Solution {
 public:
-    int search(vector<int> nums, int target)
-{
-	int n = nums.size();
-
-	for (int i = 0; i < n; i++)
-	{
-		int currElement = nums[i];
-		if (currElement == target)
-		{
-			return i;
-		}
-	}
-
-	return -1;   // if target is not present 
-}
+    int bSearch(vector<int>& nums, int target, int start, int end){
+        if(end < start) return -1;
+        int mid = (start+end)/2;
+        // cout << mid << endl;
+        if(nums[mid] == target){
+            return mid;
+        }else if(target < nums[mid]){
+            return bSearch(nums, target, start, mid-1);
+        }else{
+            return bSearch(nums, target, mid+1, end);
+        }
+    }
+    
+    int search(vector<int>& nums, int target) {
+        return bSearch(nums, target, 0, nums.size()-1);
+    }
 };
