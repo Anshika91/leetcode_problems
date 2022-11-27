4c6f694f0f36fe78b9e8d88cf0dffcb0f4a8fcba
Anshika Mishra <92078327+Anshika91@users.noreply.github.com>
2022-11-27 09:21:31 +0530

    Time: 7 ms (96.91%), Space: 10.3 MB (45.63%) - LeetHub

---
+++ 1356-sort-integers-by-the-number-of-1-bits/1356-sort-integers-by-the-number-of-1-bits.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    int count_set_bit(int x){
+        int bits = 0;
+        while(x != 0){
+            bits += x&1;
+            x >>= 1;
+        }
+        return bits;
+    }
+    vector<int> sortByBits(vector<int>& arr) {
+        vector<int> sorted;
+        for(int e : arr){
+            //first sort by set bit count and then by itself
+            //* 100000 because the max of arr is 10000
+            sorted.push_back(count_set_bit(e) * 100000 + e);
+        }
+        sort(sorted.begin(), sorted.end());
+        for(int i = 0; i < sorted.size(); i++){
+            sorted[i] = sorted[i]%100000;
+        }
+        
+        return sorted;
+    }
+};
