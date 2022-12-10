0eb8ea4afe47506a5ae354ac14e36fab01c45594
Anshika Mishra <92078327+Anshika91@users.noreply.github.com>
2022-12-10 19:18:41 +0530

    Create maximum product of spilitted binary tree.cpp
    added  a  leetcode daily challenge

---
+++ maximum product of spilitted binary tree.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    int maxProduct(TreeNode* root) {
+        static const int MOD = 1e9 + 7;
+        int64_t result = 0;
+        dfs(root, dfs(root, 0, &result), &result);
+        return result % MOD;
+    }
+
+private:
+    int64_t dfs(TreeNode *root, int64_t total, int64_t *result) {
+        if (!root) {
+            return 0;
+        }
+        int64_t subtotal = dfs(root->left, total, result) +
+                           dfs(root->right, total, result) +
+                           root->val;
+        *result = max(*result, subtotal * (total - subtotal));
+        return subtotal;
+    }
+};
