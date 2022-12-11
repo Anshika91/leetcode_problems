9a8df9369a2e12e752c855e8fa5cc2023f797dee
Anshika Mishra <92078327+Anshika91@users.noreply.github.com>
2022-12-11 09:19:20 +0530

    Create Binary tree maximum path.cpp
    added a leetcode solution

---
+++ Binary tree maximum path.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    int ans;
+    
+    int maxPathDown(TreeNode* node){
+        if(!node) return 0;
+        //max sum of path rooted at node->left, going down
+        int left = max(maxPathDown(node->left), 0);
+        //max sum of path rooted at node->right, going down
+        int right = max(maxPathDown(node->right), 0);
+        ans = max(ans, left+right+node->val);
+        //max sum of path that goes down
+        return node->val + max(left, right);
+    };
+    
+    int maxPathSum(TreeNode* root) {
+        ans = INT_MIN;
+        
+        maxPathDown(root);
+        
+        return ans;
+    }
+};
