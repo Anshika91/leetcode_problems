e18a0f4b9551c15e99d9ea655a548e4146767751
Anshika Mishra <92078327+Anshika91@users.noreply.github.com>
2022-12-20 12:45:47 +0530

    Create Is subsequence.cpp
    added a leetcode solution

---
+++ Is subsequence.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    bool isSubsequence(string s, string t) {
+        if (s.empty()) {
+            return true;
+        }
+
+        int i = 0;
+        for (const auto& c : t) {
+            if (c == s[i]) { 
+                ++i;
+            }
+            if (i == s.length()) {
+                break;
+            }
+        }
+        return i == s.length();
+    }
+};
