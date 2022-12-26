ad1b6ea062c33316525ae0294277115b31c00534
Anshika Mishra <92078327+Anshika91@users.noreply.github.com>
2022-12-26 16:52:19 +0530

    Create Increasing Triplet Subsequence.cpp
    added a leetcode solution

---
+++ Increasing Triplet Subsequence.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    bool increasingTriplet(vector<int>& nums) {
+        int min = numeric_limits<int>::max(), a = numeric_limits<int>::max(), b = numeric_limits<int>::max();
+        for (const auto& c : nums) {
+            if (min >= c) {
+                min = c;
+            } else if (b >= c) {
+                a = min, b = c;
+            } else {  // a < b < c
+                return true;
+            }
+        }
+        return false;
+    }
+};
