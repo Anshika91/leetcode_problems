9781edbd5b54fe8c61d053b54c191fa346b45d7f
Anshika Mishra <92078327+Anshika91@users.noreply.github.com>
2022-12-30 18:55:08 +0530

    Create Repeated DNA Sequence.cpp
    added a leetcode solution

---
+++ Repeated DNA Sequence.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    vector<string> findRepeatedDnaSequences(string s) {
+        set<string> ans;
+        
+        for(int i = 0; i + 10 <= s.size(); i++){
+            string pattern = s.substr(i, 10);
+            size_t found = s.find(pattern, i+1);
+            if(found != string::npos){
+                ans.insert(pattern);
+            }
+        }
+        
+        return vector<string>(ans.begin(), ans.end());
+    }
+};
