9fb9929d288f8ceccc88a773814f1113b8cb8c95
Anshika Mishra <92078327+Anshika91@users.noreply.github.com>
2023-01-02 20:17:35 +0530

    Create Detect Capital.cpp
    added a leetcode solution

---
+++ Detect Capital.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    bool detectCapitalUse(string word) {
+        if(isupper(word[0])){
+            if(word.size()<2) return true;
+            if(isupper(word[1])){
+                for(int i = 1; i < word.size(); i++){
+                    if(islower(word[i])) return false;
+                }
+            }else{
+                for(int i = 1; i < word.size(); i++){
+                    if(isupper(word[i])) return false;
+                }
+            }
+        }else{
+            for(int i = 1; i < word.size(); i++){
+                if(isupper(word[i])) return false;
+            }
+        }
+        return true;
+    }
+};
