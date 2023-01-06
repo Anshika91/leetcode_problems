4964787cf5e6058bdb425496d08236e91d6e8001
Anshika Mishra <92078327+Anshika91@users.noreply.github.com>
2023-01-06 14:59:13 +0530

    Create Delete node in a bst.cpp
    added a leetcode solution

---
+++ Delete node in a bst.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    TreeNode* deleteNode(TreeNode* root, int key) {
+        TreeNode* head = new TreeNode(0);
+        head->left = root;
+        dfs(head, root, key);
+        return head->left;
+    }
+    
+    void dfs(TreeNode* pre, TreeNode* cur, int key){
+        if(!cur) return;
+        cur->val > key ? dfs(cur, cur->left, key) : cur->val < key ? dfs(cur, cur->right, key) : helper(pre, cur);
+    }
+    
+    void helper(TreeNode* pre, TreeNode* cur){
+        TreeNode* p = cur->left;
+        while(p && p->right) p = p->right;
+        p ? p->right = cur->right : cur->left = cur->right;
+        cur == pre->left ? pre->left = cur->left : pre->right = cur->left;
+    }
+};
