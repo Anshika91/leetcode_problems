8e8646898c97a7891a361cda80fd6fa2a620dec0
Anshika Mishra <92078327+Anshika91@users.noreply.github.com>
2023-01-10 14:23:52 +0530

    Create top k frequent elements.cpp
    added a leetcode solution

---
+++ top k frequent elements.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    vector<int> topKFrequent(vector<int>& nums, int k) {
+        unordered_map<int, int> counts;
+        for (const auto& i : nums) {
+            ++counts[i];
+        }
+
+        vector<pair<int, int>> p;
+        for (auto it = counts.begin(); it != counts.end(); ++it) {
+            p.emplace_back(-(it->second), it->first);
+        }
+        nth_element(p.begin(), p.begin() + k - 1, p.end());
+
+        vector<int> result;
+        for (int i = 0; i < k; ++i) {
+            result.emplace_back(p[i].second);
+        }
+        return result;
+    }
+};
