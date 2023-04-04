b471bdca028fa1d51c87fa45527e5b0ee977d144
Anshika Mishra <92078327+Anshika91@users.noreply.github.com>
2023-04-04 21:51:08 +0530

    Create optimal partition of string.cpp
    added leetcode solution

---
+++ optimal partition of string.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    int partitionString(string s) {
+        int result = 1, left = 0;
+        vector<int> lookup(26, -1);
+        for (int i = 0; i < size(s); ++i) {
+            if (lookup[s[i] - 'a'] >= left) {
+                left = i;
+                ++result;
+            }
+            lookup[s[i] - 'a'] = i;
+        }
+        return result;
+    }
+};
